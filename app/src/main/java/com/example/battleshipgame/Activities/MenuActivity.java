759240ada60b963abb1985479ffd50aa7a980a3b
annahdigital <annah.borisova@gmail.com>
2019-12-20 23:26:30 +0300

    log out added.

---
+++ app/src/main/java/com/example/battleshipgame/Activities/MenuActivity.java
@@ -0,0 +1,70 @@
+package com.example.battleshipgame.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.battleshipgame.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private GoogleSignInClient googleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        TextView nameView = findViewById(R.id.menu_username);
+        nameView.setText(currentUser.getDisplayName());
+        Button signOutButton = findViewById(R.id.menu_log_out);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        goToLogIn();
+                    }
+                });
+    }
+
+    private void goToLogIn()
+    {
+        Intent intent = new Intent(this, LaunchActivity.class);
+        this.startActivity(intent);
+        this.finish();
+    }
+}
---
+++ app/src/main/java/com/example/battleshipgame/Grid/GridField.java
@@ -0,0 +1,19 @@
+package com.example.battleshipgame.Grid;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.view.View;
+
+/*public class GridField extends View {
+    private int cellWidth;
+    private int cellHeight;
+
+    private Paint PurplePaint = new Paint();
+    private Paint RedPaint = new Paint();
+
+    //private Grid grid;
+    private CellMode modes;
+    private Context context;
+
+
+}*/
